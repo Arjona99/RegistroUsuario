ba3596421a260edb58a957cf6a9494502e6be03d
Arjona99 <sebasarjona@outlook.com>
2021-11-07 22:02:16 -0600

    Add buttons functionality

--- app/src/main/java/com/example/registrousuario/MainActivity.kt
+++ app/src/main/java/com/example/registrousuario/MainActivity.kt
@@ -1,35 +1,79 @@
 package com.example.registrousuario
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent;
 import android.os.Bundle
 import android.view.Window
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
-    object userData {
-        var name = "";
-        var email = "";
-        var accountNumber = "";
-        object birthDate {
-            var day = 0;
-            var month = 0;
-            var year = 0;
-        }
-    }
 
     var day = 0;
     var month = 0;
     var year = 0;
 
+    // View elements
+    var etName: EditText? = null
+    var etEmail: EditText? = null
+    var etAccountNumber: EditText? = null
+    var etBirthDate: EditText? = null
+    var btNext: Button? = null
+    var btClear: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature((Window.FEATURE_NO_TITLE));
         getSupportActionBar()?.hide()
         setContentView(R.layout.activity_main)
-        var etBirthDate = findViewById<EditText>(R.id.birthDate)
-        etBirthDate.setOnClickListener{showDatePickerDialog()}
 
+        // View Elements
+        etName = findViewById<EditText>(R.id.name)
+        etEmail = findViewById<EditText>(R.id.email)
+        etAccountNumber = findViewById<EditText>(R.id.accountNumber)
+        etBirthDate = findViewById<EditText>(R.id.birthDate)
+        btNext = findViewById<Button>(R.id.bt_next)
+        btClear = findViewById<Button>(R.id.bt_clear)
+
+        etBirthDate?.setOnClickListener{showDatePickerDialog()}
+
+
+    }
+
+    fun clickClean ( view: View) {
+        etName?.setText("")
+        etEmail?.setText("")
+        etAccountNumber?.setText("")
+        etBirthDate?.setText("")
+    }
+
+    fun clickNext (view: View) {
+        if (checkAllFields()) {
+            val i = Intent(this@MainActivity, ShowUserData::class.java)
+            startActivity(i)
+        }
+    }
+
+    private fun checkAllFields (): Boolean {
+        if (etName?.length() == 0) {
+            etName?.setError("This field is required")
+            return false
+        }
+        if (etEmail?.length() == 0) {
+            etEmail?.setError("This field is required")
+            return false
+        }
+        if (etAccountNumber?.length() == 0) {
+            etAccountNumber?.setError("This field is required")
+            return false
+        }
+        if (etBirthDate?.length() == 0) {
+            etBirthDate?.setError("This field is required")
+            return false
+        }
+        return true
     }
 
     private fun showDatePickerDialog() {
---
+++ app/src/main/java/com/example/registrousuario/ShowUserData.kt
@@ -0,0 +1,4 @@
+package com.example.registrousuario
+
+class ShowUserData {
+}
